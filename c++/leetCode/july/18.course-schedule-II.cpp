f57beb3cf5fa3c3522881e0056486c9a0d578a77
08pixels <luizrodrigomarques@gmail.com>
2020-07-24 20:58:40 -0300

    adding description

--- c++/leetCode/july/18.course-schedule-II.cpp
+++ c++/leetCode/july/18.course-schedule-II.cpp
@@ -1,3 +1,13 @@
+/*
+    There are a total of n courses you have to take, labeled from 0 to n-1.
+    Some courses may have prerequisites, for example to take course 0
+    you have to first take course 1, which is expressed as a pair: [0,1]
+    Given the total number of courses and a list of prerequisite pairs,
+    return the ordering of courses you should take to finish all courses.
+    There may be multiple correct orders, you just need to return one of them.
+    If it is impossible to finish all courses, return an empty array.
+*/
+
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
