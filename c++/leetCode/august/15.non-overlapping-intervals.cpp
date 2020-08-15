8171a6b869ceaf6c45ac78c54a351b5b156cc545
08pixels <luizrodrigomarques@gmail.com>
2020-08-15 12:12:52 -0300

    leetcode august day 15

---
+++ c++/leetCode/august/15.non-overlapping-intervals.cpp
@@ -0,0 +1,36 @@
+
+bool compare(vector<int> &a, vector<int> &b) {
+    if ( a[0] ^ b[0] )
+        return a[0] < b[0];
+
+    return a[1] <= b[1];
+}
+
+class Solution {
+public:
+    
+
+    
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), compare);
+        
+        int answer = 0;
+        int l      = 0;
+        
+        for(int r = 1; r < n; ++r) {
+            
+            if ( intervals[l][1] <= intervals[r][0] ) {
+                l = r;
+                continue;
+            }
+            
+            if ( intervals[l][1] > intervals[r][1] )
+                l = r;
+            
+            ++answer;
+        }
+        
+        return answer;
+    }
+};
