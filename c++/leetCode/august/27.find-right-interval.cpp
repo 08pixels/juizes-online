f134104dabf8ef2c180acfdf8e8fd529462e8650
08pixels <luizrodrigomarques@gmail.com>
2020-08-27 23:59:01 -0300

    leetcode august day 27

--- c++/leetCode/august/27.find-right-interval.cpp
+++ c++/leetCode/august/27.find-right-interval.cpp
@@ -4,20 +4,14 @@ public:
         int size = intervals.size();
         map<int, int> index;
         
-        for(int i=0; i<size; ++i) {
-            int curr = intervals[i][0];
-            
-            if ( index.count(curr) )
-                index[curr] = min( index[curr], i );
-            else
-                index[curr] = i;
-        }
+        for(int i=0; i<size; ++i)
+            index[intervals[i][0]] = i;
         
         vector<int> answer(size, -1);
         for(int i=0; i<size; ++i) {
             auto it = index.lower_bound(intervals[i][1]);
             
-            if ( it != index.end() )
+            if ( it != index.end())
                 answer[i] = it->second;
         }
         
