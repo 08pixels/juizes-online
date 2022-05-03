93aed88cb08d0c0eac86db0e5900c067c7c3d3c1
08pixels <luiz.marques@arapiraca.ufal.br>
2022-05-03 17:31:04 -0300

    adhoc: flag

---
+++ c++/hacker-cup/2021/round-1/a1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f(string str) {
+    int change = 0;
+    int l  = 0;
+
+    for (auto &e: str) {
+        if (e == 'F') continue;
+
+        if (l && e != l)
+            ++change;
+        l = e;
+    }
+
+    return change;
+}
+
+int main() {
+    int T; cin >> T;
+
+    for (int test=0; test<T; ++test) {
+        int N; cin >> N;
+        string W; cin >> W;
+
+        cout  << "Case #" << 1+test << ": " << f(W) << endl;
+    }
+    return 0;
+}
