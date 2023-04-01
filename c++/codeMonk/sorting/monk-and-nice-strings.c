71e0f1400ac8fbad3a90199a821546871b080728
08pixels <luiz.marques@arapiraca.ufal.br>
2023-03-31 23:22:13 -0300

    insertion sort: basic

--- c++/codeMonk/sorting/monk-and-nice-strings.c
+++ c++/codeMonk/sorting/monk-and-nice-strings.c
@@ -1,25 +1,23 @@
-https://www.hackerearth.com/practice/codemonk/?purpose=login&source=code_monk&update=google
-
 #include <stdio.h>
 #include <string.h>
 
 int main(){
-	int num;
-	scanf("%d", &num);
+  int num;
+  scanf("%d", &num);
 
-	char v[num  + 1][15];
+  char v[num  + 1][15];
 
-	for(int i=0; i<num; ++i) {
-		char key[15]; scanf("%s", key);
-		
-		int k = i - 1;
+  for(int i=0; i<num; ++i) {
+    char key[15]; scanf("%s", key);
+    
+    int k = i - 1;
 
-		while(k >= 0 && strcmp(v[k], key) >= 0) {
-            strcpy(v[k + 1], v[k]);
-			--k;
-		}
+    while(k >= 0 && strcmp(v[k], key) >= 0) {
+      strcpy(v[k + 1], v[k]);
+      --k;
+    }
 
-        strcpy(v[k+1], key);
-		printf("%d\n", k + 1);
-	}
+    strcpy(v[k+1], key);
+    printf("%d\n", k + 1);
+  }
 }
